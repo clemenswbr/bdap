db0b2e1eb44f03305b81f8ba19f1d72463446504
clemenswbr <ridethree@gmx.de>
2024-09-25 11:23:11 +0200

    changes

--- run_convert.py
+++ run_convert.py
@@ -4,12 +4,16 @@ import subprocess
 
 run_numbers = [int(n[8:-3]) for n in glob.glob('../soils*')]
 
+irri100 = '../../../../MODEL/DAYCENT/RUN/DayC/irri.100'
+harv100 = '../../../../MODEL/DAYCENT/RUN/DayC/harv.100'
+omad100 = '../../../../MODEL/DAYCENT/RUN/DayC/omad.100'
+
 for run_number in run_numbers:
 
     print(run_number)
     print(f'../soils{run_number}.in', f'./test/{run_number}_site.xml')
     print(f'../meteo{run_number}.wth', f'./test/{run_number}_climate.txt', f'../site{run_number}.100')
-    print(f'../mgt{run_number}.evt', f'./test/{run_number}_mana.xml', 'omad.100', 'harv.100', 'irri.100', 'dc_ldndc_lookup.csv')
+    print(f'../mgt{run_number}.evt', f'./test/{run_number}_mana.xml', f'dirComad{run_number}.in', harv100, irri100, 'dc_ldndc_lookup.csv')
     print(run_number, f'./test/{run_number}_setup.xml')
     print(run_number, f'./test/{run_number}.ldndc', f'./test/{run_number}_mana.xml')
     print('_______________________________')
