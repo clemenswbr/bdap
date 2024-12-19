7b8eeb2639f26da1ba4fd28af6c5327174755ecd
clemenswbr <ridethree@gmx.de>
2024-12-19 12:52:49 +0100

    changed create lat long

--- create_lat_long.py
+++ create_lat_long.py
@@ -11,10 +11,20 @@ print(file_in, ' ---> ', file_out)
 lat_long = rasterio.open(file_in)
 
 lat = lat_long.read(1)
-long = lat_long.read(1)
-index = np.arange(1, len(lat) + 1, 1)
+long = lat_long.read(2)
+#index = np.arange(1, len(lat) + 1, 1)
 
-df = pd.DataFrame({'index':index, 'lat':lat, 'long':long})
+lat_t = []
+long_t = []
+
+for lat_i in range(lat.shape[0]):
+    for long_i in range(lat.shape[1]):
+        #print(lat_i, long_i)
+        lat_t.append(lat[lat_i,long_i])
+        long_t.append(long[lat_i,long_i])
+
+
+df = pd.DataFrame({'index':np.arange(0, len(lat_t)), 'lat':lat_t, 'long':long_t})
 df.to_csv(file_out)
 
 print('DONE')
