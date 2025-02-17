dec4edebf0ce12a3d8a89941ca6bfed292b2fa93
clemenswbr <ridethree@gmx.de>
2025-02-17 15:14:32 +0100

    c

--- catch_results.py
+++ catch_results.py
@@ -1,5 +1,4 @@
 from netCDF4 import Dataset
-import sys
 import os
 import numpy as np
 import pandas as pd
@@ -23,25 +22,29 @@ ncfile.createDimension('long', len(long))
 ncfile.createDimension('time', None)
 
 #Create variables
-row = ncfile.createVariable('lat', np.float32, ('lat',))
-row.units = '[]'
-col = ncfile.createVariable('long', np.float32, ('long',))
-col.units = '[]'
+lat = ncfile.createVariable('lat', np.float32, ('lat',))
+lat.units = 'latitude'
+long = ncfile.createVariable('long', np.float32, ('long',))
+long.units = 'longitude'
 time = ncfile.createVariable('time', np.float32, ('time'))
 times = pd.date_range(pd.to_datetime('2011-01-04'), pd.to_datetime('2024-12-31'), freq='w')
 time[:] = times
 time.units = 'Week'
 n2o = ncfile.createVariable('n2o', np.float64, ('time', 'lat', 'long'))
 n2o.units = 'kg*km^-2*week^-1'
-n2o[:,:,:] = -99.99
 
-#Fill netCDF
-for r in range(len(lat)):
-    for c in range(len(long)):
+#Create empty array to fill with values
+arr = np.full((len(times, lat_long.height, lat_long.width)), -99.99)
+
+#Fill array
+for r in range(lat_long.height):
+    for c in range(lat_long.width):
         print(r, c)
         #Get lat and long
         lat_df = lat[r,c]
         long_df = long[r,c]
+        if any(lat_df == lat_long.nodata, long_df == lat_long.nodata):
+            continue
         #Check if file exists
         try:
             df = pd.read_csv(f'test_ldndc_output/{r}_{c}_output/{r}_{c}_soilchemistry-daily.txt', sep='\t')[['datetime', 'dN_n2o_emis[kgNha-1]']]
@@ -49,14 +52,13 @@ for r in range(len(lat)):
             continue
         #Read ldndc output, aggregate to weekly resolution, convert from kgNha-1 to kgNkm-2
         df['datetime'] = pd.to_datetime(df['datetime'])
-        #df = df.replace(-99.99, 0)
         df.set_index('datetime', inplace=True)
         df_weekly = df.resample('W').sum()
         df_weekly['dN_n2o_emis[kgNha-1]'] = df_weekly['dN_n2o_emis[kgNha-1]'] * 0.01
-        #Write to netCDF if output is complete
+        #Write to array if output is complete
         if len(df_weekly) == len(times):
-            n2o[:,lat_df,long_df] = df_weekly['dN_n2o_emis[kgNha-1]']
-        else:
-            n2o[:,lat_df,long_df] = -99.99   
+            arr[:,lat_df,long_df] = df_weekly['dN_n2o_emis[kgNha-1]']
 
+#write to netCDF
+n2o[:,:,:] = arr
 ncfile.close()
