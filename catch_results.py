a5467582c6a1d23c5b9f16279b3873c3bd0b519a
clemenswbr <ridethree@gmx.de>
2025-02-19 15:53:20 +0100

    c

--- catch_results.py
+++ catch_results.py
@@ -32,6 +32,7 @@ for r in range(n_row):
         df_weekly['dN_n2o_emis[kgNha-1]'] = df_weekly['dN_n2o_emis[kgNha-1]'] * 0.01
         #Write to file if output is complete
         if len(df_weekly) == n_time:
+            print('Writing to ncfile', df_weekly['dN_n2o_emis[kgNha-1]'][0])
             ncfile['n2o'][:,r,c] = df_weekly['dN_n2o_emis[kgNha-1]']
 
 ncfile.close()
