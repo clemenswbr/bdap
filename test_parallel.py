d46f9b0d0880a1eb3ab1eb44041fa42a06eb7e78
clemenswbr <ridethree@gmx.de>
2025-03-06 11:05:31 +0100

    c

--- test_parallel.py
+++ test_parallel.py
@@ -6,15 +6,14 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('-index', '--index', help='No index found', dest='index', required=True, type=int)
 parser.add_argument('-cores', '--cores', help='No cores found', dest='cores', required=True, type=int)
-index = parser.parse_args().index
 cores = parser.parse_args().cores
+index = parser.parse_args().index if cores > 1 else 0
 
 ldndc_files = glob.glob('/eos/jeodpp/data/projects/SOIL-NACA/MODEL4/test_ldndc/*.ldndc')
 ldndc_files = np.array_split(ldndc_files, cores)[index]
 
 with open(f'/eos/jeodpp/data/projects/SOIL-NACA/MODEL4/test_{index}.txt', 'w') as f:
     for line in ldndc_files:
-        f.write(line + '\n')
-        f.write('test')
+        f.write(f'./ldndc -c ldndc_test.conf {line}\n')
 
 f.close()
