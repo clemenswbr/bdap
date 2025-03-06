7c7c1a60984f2de57229f17292fe37404ffc0a09
clemenswbr <ridethree@gmx.de>
2025-03-06 11:24:12 +0100

    c

--- test_parallel.py
+++ test_parallel.py
@@ -1,7 +1,10 @@
 import argparse
+import subprocess
 import glob
 import numpy as np
 
+ldndc_executable = '/eos/jeodpp/data/projects/SOIL-NACA/MODEL4/LDNDC/ldndc'
+ldndc_conf = '/eos/jeodpp/data/projects/SOIL-NACA/MODEL4/ldndc_test.conf'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-index', '--index', help='No index found', dest='index', required=True, type=int)
@@ -12,8 +15,7 @@ index = parser.parse_args().index if cores > 1 else 0
 ldndc_files = glob.glob('/eos/jeodpp/data/projects/SOIL-NACA/MODEL4/test_ldndc/*.ldndc')
 ldndc_files = np.array_split(ldndc_files, cores)[index]
 
-with open(f'/eos/jeodpp/data/projects/SOIL-NACA/MODEL4/test_{index}.txt', 'w') as f:
-    for line in ldndc_files:
-        f.write(f'./ldndc -c ldndc_test.conf {line}\n')
+for sim in ldndc_files:
+    subprocess.run(f'{ldndc_executable} {ldndc_conf} {sim}'.split())
 
 f.close()
